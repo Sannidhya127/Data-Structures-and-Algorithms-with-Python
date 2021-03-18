6cf5761becc0857941643f7c6ef9d9152952b789
Sannidhya127 <shubhradasgupta8@gmail.com>
2021-03-18 14:55:31 +0530

    Completed the factorial code and will now make fibonacci series with recursion

--- factorial.py
+++ factorial.py
@@ -1,15 +1,30 @@
+import sys
+
+# sys.setrecursionlimit(5000)
+
 # My method of finding factorial
 
-num = int(input("Enter the number:"))
-factorial = 1
-for i in range(1, num+1):
-    factorial = factorial * i
 
-print("Factorial of a Given Number:", factorial)
+def factorial():
+    num = int(input("Enter the number:"))
+    factorial = 1
+    for i in range(1, num+1):
+        factorial = factorial * i
+
+    print("Factorial of a Given Number:", factorial)
 
 
 def factorialRecursive(n):
-    print(n * factorialRecursive(n-1))
+    # assert n>=0 and int(n) == n, "The number has to be a positive integer"  (Can use this instead of the elif also)
+    if n in [0, 1]:
+        return 1
+    elif n < 0:
+        print("Can't find factorial of negative integers")
+    elif "." in str(n):
+        print("No decimal integer supported")
+    else:
+        return n * factorialRecursive(n-1)
 
 
-factorialRecursive(5)
+print(factorialRecursive(4))
+factorial()
